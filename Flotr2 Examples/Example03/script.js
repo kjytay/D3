63d0d7de214c3e6d0a61affc0ae252f0fa342177
Kenneth Tay <kjytay@gmail.com>
2015-11-19 10:20:15 +0800

    Added Flotr2 Examples 03-05.

---
+++ Flotr2 Examples/Example03/script.js
@@ -0,0 +1,22 @@
+// Source: Stephen A. Thomas, "Data Visualization with Javascript."
+
+// Data for Chart
+var data = [
+    {data: [[0,22.4]], label: "Extreme Poverty"},
+    {data: [[1,77.6]]}
+    ];
+
+window.onload = function () {
+	Flotr.draw(
+	    document.getElementById("chart1"),
+	    data,
+	    {
+	    	title: "How Much of the World Lives on $1.25/Day?",
+	    	pie: { show: true },
+	    	yaxis: { showLabels: false },
+	    	xaxis: { showLabels: false },
+	    	grid: { horizontalLines: false,
+	    	        verticalLines: false }
+	    }
+	);
+}
--- Flotr2 Examples/README.md
+++ Flotr2 Examples/README.md
@@ -2,4 +2,6 @@
 
 1. 2 bar chart examples.
 2. Line chart example.
-
+3. Pie chart example.
+4. Scatter chart example.
+5. Bubble chart example.
